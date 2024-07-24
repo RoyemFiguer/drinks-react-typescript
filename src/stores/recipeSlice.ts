1ceefec2ddaa338c95f91c5a4b85de774315f9a6
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-24 13:55:20 -0600

    Passing the id to query details

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,13 +1,14 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipes } from "../services/RecipeService"
 
-import type { Categories, Drinks, SearchFilter } from "../types"
+import type { Categories, Drink, Drinks, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
     categories: Categories,
     drinks: Drinks,
     fetchCategories: () => Promise<void>,
     searchRecipes: (searchFilter : SearchFilter) => Promise<void>
+    selectRecipe: (id: Drink['idDrink']) => Promise<void>
 }
 
 export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
@@ -28,5 +29,8 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
         set({
             drinks
         })
+    },
+    selectRecipe: async (id)=>{
+        console.log('Desde selectRecipe')
     }
 })

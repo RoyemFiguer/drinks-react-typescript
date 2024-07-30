1d512cf34288556fa8abd250794df7e50fac6fe8
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-30 12:13:41 -0600

    Using API to get drink ingredients by ID

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CategoriesAPIResponseSchema, DrinksAPIResponse } from "../utils/recipes-schema"
+import { CategoriesAPIResponseSchema, DrinksAPIResponse, RecipeAPIResponseSchema } from "../utils/recipes-schema"
 import { SearchFilter } from "../types"
 
 export async function getCategories(){
@@ -21,4 +21,14 @@ export async function getRecipes(filters: SearchFilter){
     if(result.success){
         return result.data
     }
+}
+
+export async function getRecipeById(id: Drink['idDrink']){
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    const {data} = await axios(url)
+    const result = RecipeAPIResponseSchema.safeParse(data.drinks[0]);
+    if(result.success){
+        return result.data
+    }
+
 }
--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
 
 import type { Categories, Drink, Drinks, SearchFilter } from "../types"
 
@@ -31,6 +31,7 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
         })
     },
     selectRecipe: async (id)=>{
-        console.log('Desde selectRecipe')
+        const selectRecipe= await getRecipeById(id)
+        console.log(selectRecipe)
     }
 })

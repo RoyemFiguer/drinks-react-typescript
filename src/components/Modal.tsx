f482bdb9ae3c8aafdc1585fe4b0516306aaf2e80
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-08-01 12:53:28 -0600

    Adding to favorites

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,8 @@ export default function Modal() {
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+    const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+    const favoriteExists = useAppStore((state) => state.favoriteExists)
 
     const renderIngredients = () =>{
         const ingredients : JSX.Element[] = []
@@ -84,7 +86,8 @@ export default function Modal() {
                     <button
                         type='button'
                         className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500'
-                    >Agregar a Favoritos</button>
+                        onClick={() => handleClickFavorite(selectedRecipe)}
+                    >{favoriteExists(selectedRecipe.idDrink) ? 'Eliminar Favorito' : 'Agregar a Favoritos'}</button>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
